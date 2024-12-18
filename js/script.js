a1489a8e58c00acf42d6d9ac689d2bc9b73f2f9f
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-12-18 21:12:50 +0545

    onClick event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const btn = document.getElementById('btn');
 const body = document.querySelector('body');
-
+const div = document.querySelector('.myDiv');
+const btn1 = document.querySelector('.btn1');
 
 
 const btnClick = (color) => {
-    body.style.backgroundColor = color
+    body.style.backgroundColor = color;
 }
+
+div.innerHTML = "Hello I am a Div";
+
+btn1.addEventListener("click", () => {
+    btn1.style.backgroundColor = "red";
+})
